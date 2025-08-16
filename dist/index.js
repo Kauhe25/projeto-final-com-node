0485ee78252427291008e0d201cf225a805c731c
kauhe <kauhe25@gmail.com>
2025-08-16 18:01:08 -0300

    feat: adicionando rota raiz

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
-console.log('teste funciona!!!');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const porta = 3030;
+// Carregando a rota raiz
+app.get('/', (req, res) => {
+    res.send('Executando a node js API Rest');
+});
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}.`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('teste funciona!!!');
+import express, {Request, Response} from 'express';
+
+const app = express();
+const porta = 3030;
+
+// Carregando a rota raiz
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Executando a node js API Rest')
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando na porta ${porta}.`)
+});
