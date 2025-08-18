01540c972a8907248f8a38fae9d503d8ca93101d
kauhe <kauhe25@gmail.com>
2025-08-18 10:52:08 -0300

    feat: Criando conexão com o BD e listando registros

--- src/controllers/defaultControllers.ts
+++ src/controllers/defaultControllers.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from 'express';
+import conexao from '../services/connection';
 
 export const rootResponse = (req: Request, res: Response): void => {
   res.send('Executando a node js API Rest com Rota')
@@ -6,4 +7,11 @@ export const rootResponse = (req: Request, res: Response): void => {
 
 export const pingResponse = (req: Request, res: Response): void => {
   res.send('Pong')
+}
+
+export const listaDepartamentos = async (req: Request, res: Response) => {
+  console.log('GET Departamentos');
+
+  const [rows] = await conexao.query('SELECT * FROM DEPARTAMENTOS');
+  res.json(rows);
 }
--- src/routes/defaultRoutes.ts
+++ src/routes/defaultRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { rootResponse, pingResponse } from "../controllers/defaultControllers";
+import { rootResponse, pingResponse, listaDepartamentos } from "../controllers/defaultControllers";
 
 const defaultRoutes = Router();
 defaultRoutes.get('/', rootResponse);
 defaultRoutes.get('/ping', pingResponse);
+defaultRoutes.get('/departamentos', listaDepartamentos);
 
 export default defaultRoutes;
